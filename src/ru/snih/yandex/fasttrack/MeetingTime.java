5d5fa96a14d9ce85d3156010ccece96328222937
Sneech <Snih@yandex.ru>
2023-09-17 01:00:21 +0300

    check for interval duration

--- src/ru/snih/yandex/fasttrack/MeetingTime.java
+++ src/ru/snih/yandex/fasttrack/MeetingTime.java
@@ -1,18 +1,22 @@
 package ru.snih.yandex.fasttrack;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class MeetingTime {
     public static void main(String[] args) {
-        try (Scanner scan = new Scanner(System.in)) {
+        try (Scanner scan = new Scanner(new File("input.txt"))) {
             int duration = scan.nextInt();
             Interval[] a = readIntervals(scan);
             Interval[] b = readIntervals(scan);
             Interval meetingTime = new Interval(-1, -1);
             A:
             for (int i = 0; i < a.length; i++) {
+                if (a[i].getDuration() < duration) continue;
                 B:
                 for (int j = 0; j < b.length; j++) {
+                    if (b[j].getDuration() < duration) continue;
                     if (a[i].end <= b[j].start) break B;
                     if (a[i].start + duration <= b[j].end && b[j].start + duration <= a[i].end) {
                         int meetingStart = Math.max(a[i].start, b[j].start);
@@ -22,6 +26,8 @@ public class MeetingTime {
                 }
             }
             System.out.println(meetingTime);
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
         }
     }
 
@@ -41,6 +47,10 @@ public class MeetingTime {
         public Interval(int start, int end) {
             this.start = start;
             this.end = end;
+        }
+
+        public int getDuration() {
+            return end - start;
         }
 
         @Override
